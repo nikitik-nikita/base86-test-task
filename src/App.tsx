3f52e2d5b12f5e9a751ad181c330b4911b633d4e
nikitik-nikita <nik.ikraaa@gmail.com>
2022-07-12 20:55:24 +0200

    feat: displayed data in a user-friendly format;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card/Card";
+import {
+  fishingRegulations,
+  IFishingRegulations,
+} from "./constants/fishingRegulations";
+import { generateRandValue } from "./helpers/generateRandValue";
+import { handeConvertToInches } from "./helpers/inchesConverter";
 
-function App() {
+const handleUpdateData = (
+  data: IFishingRegulations[]
+): IFishingRegulations[] => {
+  let updatedData: IFishingRegulations[] = data;
+
+  updatedData.forEach((el) => {
+    el.id = generateRandValue();
+
+    if (el.nicknames === null) {
+      el.nicknames = "None";
+    }
+
+    if (Array.isArray(el.nicknames)) {
+      el.nicknames = el.nicknames.join(" ");
+    }
+
+    el.seasons.forEach((item) => {
+      item.id = generateRandValue();
+
+      if (item.opening === null) {
+        item.opening = "Open Season";
+      }
+
+      if (item.close === null) {
+        item.close = "Open Season";
+      }
+
+      if (item.minimumSize === null) {
+        item.minimumSize = "No size limit";
+      }
+
+      if (item.maximumSize === null) {
+        item.maximumSize = "No size limit";
+      }
+
+      if (item.bagLimit === null) {
+        item.maximumSize = "No bag limit";
+      }
+
+      if (typeof item.maximumSize === "number") {
+        item.maximumSize = handeConvertToInches(item.maximumSize);
+      }
+
+      if (typeof item.minimumSize === "number") {
+        item.minimumSize = handeConvertToInches(item.minimumSize);
+      }
+    });
+  });
+
+  return updatedData;
+};
+
+const data = handleUpdateData(fishingRegulations);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map((elem) => (
+        <Card
+          key={elem.id}
+          title={elem.species}
+          subtitle={elem.nicknames as string}
+          content={elem.seasons}
+        />
+      ))}
+    </>
   );
-}
+};
 
 export default App;
